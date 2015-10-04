93e7f8aac9abdaf109392717469050933962f819
Zhonghua Xi <xizhonghua@gmail.com>
2015-10-04 16:48:29 -0400

    add rmi.client

---
+++ GameServer/src/org/xiaohuahua/game/rmi/Client.java
@@ -0,0 +1,150 @@
+package org.xiaohuahua.game.rmi;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import org.xiaohuahua.game.common.Config;
+import org.xiaohuahua.game.common.GameObject;
+import org.xiaohuahua.game.common.GameWorld;
+import org.xiaohuahua.game.common.Player;
+import org.xiaohuahua.game.common.Scene;
+
+public class Client {
+
+  private Scene scene;
+  private String name;
+  private String token;
+  private GameWorld world;
+  private Player player;
+  private RemoteServer server;
+
+  public Client(String name, RemoteServer server) {
+    this.name = name;
+    this.server = server;
+    this.scene = new Scene("Client");
+  }
+
+  public void run() throws RemoteException {
+    // step 1, enter the game
+    this.token = this.server.enterGame(this.name);
+    System.out.println("[Client] Entered game!");
+
+    this.update();
+    this.render();
+
+    Client self = this;
+
+    scene.addKeyListener(new KeyListener() {
+
+      @Override
+      public void keyTyped(KeyEvent e) {
+      }
+
+      @Override
+      public void keyReleased(KeyEvent e) {
+      }
+
+      @Override
+      public void keyPressed(KeyEvent e) {
+
+        try {
+
+          switch (e.getKeyCode()) {
+          case KeyEvent.VK_ESCAPE:
+            self.leave();
+            break;
+          case KeyEvent.VK_UP:
+            self.move(0, -1);
+            break;
+          case KeyEvent.VK_DOWN:
+            self.move(0, 1);
+            break;
+          case KeyEvent.VK_LEFT:
+            self.move(-1, 0);
+            break;
+          case KeyEvent.VK_RIGHT:
+            self.move(1, 0);
+            break;
+          case KeyEvent.VK_SPACE:
+            self.openChest();
+            break;
+          }
+          self.render();
+        } catch (Exception exeption) {
+          System.out.println("Error: " + exeption);
+          exeption.printStackTrace();
+        }
+      }
+    });
+  }
+
+  private void render() {
+    this.scene.repaint();
+    this.scene.setTitle(this.player.toString());
+  }
+
+  private void move(int dx, int dy) throws RemoteException {
+    boolean moved = this.server.move(token, dx, dy);
+    this.update();
+
+    if (moved) {
+      System.out.println("[Client] Moved to " + this.player.getX() + " , "
+          + this.player.getY());
+    }
+  }
+
+  private void openChest() throws RemoteException {
+    int value = this.server.open(token);
+    this.update();
+
+    if (value > 0) {
+      System.out.println("[Client] Opened Chest! Awarded " + value + "!");
+    }
+  }
+
+  private void leave() throws RemoteException {
+    this.server.leaveGame(token);
+    System.out.println("[Client] Left game!");
+    System.exit(0);
+  }
+
+  private void update() throws RemoteException {
+    List<GameObject> objects = this.server.getObjects();
+
+    // init world
+    this.world = new GameWorld();
+    this.world.initWithObjects(objects);
+    // get myself
+    player = this.world.getPlayerByName(this.name);
+
+    this.scene.setMap(world);
+    this.scene.setMe(player);
+  }
+
+  public static void main(String[] args) throws RemoteException {
+
+    if (args.length < 1) {
+      System.out.print(
+          "Usage: java " + Client.class.getName() + " playerName [server]");
+    }
+
+    String name = args[0];
+    String serverName = args.length > 1 ? args[1] : "localhost";
+    String fullServiceName = "rmi://" + serverName + "/"
+        + Config.GAME_SERVICE_NAME;
+    RemoteServer server = null;
+
+    try {
+      server = (RemoteServer) Naming.lookup(fullServiceName);
+    } catch (Exception e) {
+      System.out.print("Failed to find game service at " + fullServiceName);
+      e.printStackTrace();
+    }
+
+    Client client = new Client(name, server);
+    client.run();
+  }
+}
