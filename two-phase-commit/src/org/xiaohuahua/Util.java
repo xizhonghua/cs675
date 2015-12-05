311e273822aacd9646240ee9e89b0070f40ad9bf
Zhonghua Xi <xizhonghua@gmail.com>
2015-12-05 16:58:54 -0500

    update

--- two-phase-commit/src/org/xiaohuahua/Util.java
+++ two-phase-commit/src/org/xiaohuahua/Util.java
@@ -14,34 +14,6 @@ import javax.xml.bind.Marshaller;
 import com.google.gson.Gson;
 
 public class Util {
-  /** Read the object from Base64 string. */
-  @SuppressWarnings("unchecked")
-  public static <T> T fromString(String s) {
-    try {
-      byte[] data = Base64.getDecoder().decode(s);
-      ObjectInputStream ois = new ObjectInputStream(
-          new ByteArrayInputStream(data));
-      Object o = ois.readObject();
-      ois.close();
-      return (T) o;
-    } catch (Exception e) {
-      return null;
-    }
-
-  }
-
-  /** Write the object to a Base64 string. */
-  public static String toString(Serializable o) {
-    try {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      ObjectOutputStream oos = new ObjectOutputStream(baos);
-      oos.writeObject(o);
-      oos.close();
-      return Base64.getEncoder().encodeToString(baos.toByteArray());
-    } catch (Exception e) {
-      return null;
-    }
-  }
 
   public static String toJSON(Object o) {
     Gson g = new Gson();
